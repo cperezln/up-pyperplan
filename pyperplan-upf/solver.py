0d991b9522a552af7c193379a5a5ed660ef601d8
Luca Framba <framba@fbk.eu>
2021-09-12 12:38:13 +0200

    Parse of domain and problem. Still has issues

--- pyperplan-upf/solver.py
+++ pyperplan-upf/solver.py
@@ -17,7 +17,7 @@ import pyperplan.search as search
 import os
 import subprocess
 import re
-from typing import List, Dict, Tuple
+from typing import List, Dict, Set, Tuple
 import upf
 from upf.problem_kind import ProblemKind
 from upf.problem import Problem
@@ -31,7 +31,8 @@ from upf.plan import ActionInstance, SequentialPlan
 
 from pyperplan.pddl.pddl import Action as PyperplanAction
 from pyperplan.pddl.pddl import Type as PyperplanType
-from pyperplan.pddl.pddl import Predicate
+from pyperplan.pddl.pddl import Problem as PyperplanProblem
+from pyperplan.pddl.pddl import Predicate, Effect, Domain
 
 from pyperplan.pddl.parser import DomainDef, ProblemDef, InitStmt, GoalStmt, ActionStmt, Variable
 from pyperplan.pddl.parser import Formula, Keyword, RequirementsStmt, PredicatesStmt, PredicateInstance
@@ -64,22 +65,10 @@ class SolverImpl(upf.Solver):
     #     return plan
 
     def solve(self, problem: Problem):
-        domAST = self.parse_domain(problem)
-        # initialize the translation visitor
-        visitor = TraversePDDLDomain()
-        # and traverse the AST
-        domAST.accept(visitor)
-        # finally return the pddl.Domain
-        dom = visitor.domain
-        probAST = self.parse_problem(dom, problem)
+        dom = self.parse_domain(problem)
+        prob = self.parse_problem(dom, problem)
         search = SEARCHES["bfs"]
-
-        # initialize the translation visitor
-        visitor = TraversePDDLProblem(dom)
-        # and traverse the AST
-        probAST.accept(visitor)
-        # finally return the pddl.Problem
-        task = _ground(visitor.get_problem())
+        task = _ground(prob)
         heuristic = None
         # if not heuristic_class is None:
         #     heuristic = heuristic_class(task)
@@ -97,35 +86,39 @@ class SolverImpl(upf.Solver):
         param = tuple(problem.object(o_name) for o_name in list_str[1:])
         return ActionInstance(action, param)
 
-    def parse_problem(self, domain: DomainDef, problem: Problem) -> ProblemDef:
-        objects = [self._parse_object(o) for o in problem.all_objects()]
-        init = self._parse_initial_values(problem)
-        goal_formula = self._converter.convert_effect(problem.env.expression_manager.And(problem.goals()))
-        goal = GoalStmt(goal_formula)
-        return ProblemDef(problem.name(), domain.name, objects, init, goal)
+    def parse_problem(self, domain: Domain, problem: Problem) -> PyperplanProblem:
+        objects: Dict[str, PyperplanType] = {o.name(): self._parse_type(o.type(), self._object_pyp_type) for o in problem.all_objects()}
+        init: List[Predicate] = self._parse_initial_values(problem)
+        goal: List[Predicate] = self._parse_goal(problem)
+        return ProblemDef(problem.name(), domain, objects, init, goal)
+
+    def _parse_goal(self, problem: Problem) -> List[Predicate]:
+        p_l: List[Predicate] = []
+        for f in problem.goals():
+            assert f.is_fluent_exp()
+            obj_l: List[Tuple[str, Tuple[PyperplanType]]] = []
+            for o in f.args():
+                obj_l.append((o.object().name(), (self._parse_type(o.object().type(), self._object_pyp_type), )))
+            p_l.append(Predicate(f.fluent().name(), obj_l))
+            return p_l
 
 
-    def _parse_initial_values(self, problem: Problem) -> InitStmt:
-        pi_l: List[PredicateInstance] = []
+    def _parse_initial_values(self, problem: Problem) -> List[Predicate]:
+        p_l: List[Predicate] = []
         for f, v in problem.initial_values().items():
             if v != problem.env.expression_manager.TRUE():
                 assert not v.bool_constant_value()
                 continue
-            obj_l: List[str] = []
+            obj_l: List[Tuple[str, PyperplanType]] = []
             for o in f.args():
-                obj_l.append(o.object().name())
-            pi_l.append(PredicateInstance(f.fluent().name(), obj_l))
-        return InitStmt(pi_l)
-
-
+                obj_l.append((o.object().name(), self._parse_type(o.object().type(), self._object_pyp_type)))
+            p_l.append(Predicate(f.fluent().name(), obj_l))
+            return p_l
 
     def _parse_object(self, obj: UpfObject) -> PyperplanObject:
         return PyperplanObject(obj.name(), obj.type().name())
 
-    def parse_domain(self, problem: Problem) -> DomainDef:
-        keywords: List[Keyword] = [Keyword('strips')]
-        if problem.kind().has_flat_typing(): # type: ignore
-            keywords.append(Keyword('typing'))
+    def parse_domain(self, problem: Problem) -> Domain:
         if problem.kind().has_negative_conditions(): # type: ignore
             raise
         if problem.kind().has_disjunctive_conditions(): # type: ignore
@@ -144,45 +137,44 @@ class SolverImpl(upf.Solver):
         pyperplan_types = [self._object_pyp_type] + [self._parse_type(t, self._object_pyp_type) for t in problem.user_types().values() if t.name() != "object"]
         predicates: Dict[str, Predicate] = {}
         for n, f in problem.fluents().items():
-            count = 0
             #predicate_signature
-            pred_sign: List[Tuple[str, List[PyperplanType]]] = []
-            for t in f.signature():
-                pred_sign.append(tuple(f'a_{count}', [self._parse_type(t.name(), self._object_pyp_type)]))
-                count = count + 1
-            predicates.append(Predicate(n, pred_sign))
+            pred_sign: List[Tuple[str, Tuple[PyperplanType]]] = []
+            for _, t in enumerate(f.signature()):
+                pred_sign.append((f'a_{_}', (self._parse_type(t, self._object_pyp_type), )))
+            predicates[n] = Predicate(n, pred_sign)
         actions: Dict[str, PyperplanAction] = {a.name(): self._parse_action(a, problem.env) for a in problem.actions().values()}
-        return DomainDef(f'domain_{problem.name()}', RequirementsStmt(keywords), pyperplan_types, PredicatesStmt(predicates),  actions, None)
+        return Domain(f'domain_{problem.name()}', pyperplan_types, predicates,  actions)
 
     def _parse_action(self, action: Action, env) -> PyperplanAction:
         #action_signature
-        act_sign: List[Tuple[str, Tuple[PyperplanType, ...]]] = [tuple(p.name(),
-            tuple(self._parse_type(p.type(), self._object_pyp_type))) for p in action.parameters()]
+        act_sign: List[Tuple[str, Tuple[PyperplanType, ...]]] = [(p.name(),
+            (self._parse_type(p.type(), self._object_pyp_type), )) for p in action.parameters()]
         precond: List[Predicate] = []
+
         for p in action.preconditions():
-            if p.is_fluent():
-                precond.append(Predicate(p.fluent().name(), ))
-            elif p.is_parameter_exp():
-                #NOTE Fondamentalmente trattano i parametri delle azioni come fossero dei fluenti!!!
-
-        return PyperplanAction(action.name(), act_sign, , EffectStmt(effect))
-
-    def _parse_effects(self, action: Action, env) -> FNode:
-        effects = []
-        for e in action.effects():
-            if e.is_conditional():
-                assert False #NOTE RAISEEEE
-            if not e.value().is_bool_constant():
-                assert False #NOTE raise
-            if e.value().bool_constant_value():
-                effects.append(e.fluent())
+            if p.is_fluent_exp():
+                signature: List[Tuple[str, Tuple[PyperplanType, ...]]] = [(f'a_{_}',
+                                        (self._parse_type(type, self._object_pyp_type), ))
+                                        for _, type in enumerate(p.fluent().signature())]
+                precond.append(Predicate(p.fluent().name(), signature))
             else:
-                effects.append(env.expression_manager.Not(e.fluent()))
-        return env.expression_manager.And(effects)
-
-    def _parse_action_parameter(self, parameter: ActionParameter) -> Variable:
-        t = self._parse_type(parameter.type())
-        return Variable(parameter.name(), [parameter.type().name()])
+                #preconditions must be a list of fluents.
+                raise
+        effect = Effect()
+        add_set: Set[Predicate] = set()
+        del_set: Set[Predicate] = set()
+        for e in action.effects():
+            params: List[Tuple[str, Tuple[PyperplanType, ...]]] = [(p.parameter().name(),
+                                        (self._parse_type(p.parameter().type(), self._object_pyp_type), ))
+                                        for p in e.fluent().args()]
+            assert not e.is_conditional()
+            if e.value().bool_constant_value():
+                add_set.add(Predicate(e.fluent().fluent().name(), params))
+            else:
+                del_set.add(Predicate(e.fluent().fluent().name(), params))
+        effect.addlist = add_set
+        effect.dellist = del_set
+        return PyperplanAction(action.name(), act_sign, precond, effect)
 
     def _parse_type(self, type: UpfType, parent: PyperplanType) -> PyperplanType:
         assert type.is_user_type()

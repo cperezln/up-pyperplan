96f749ce8b5182f945bde31622c8d410caa73d57
Luca Framba <framba@fbk.eu>
2021-11-25 10:56:14 +0100

    Moved code for versioning into the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages # type: ignore
-import upf_pyperplan
 
 
 long_description=\
@@ -13,8 +12,33 @@ long_description=\
     equivalent data structures between UPF and Pyperplan.
 """
 
+VERSION = (0, 0, 1, "dev", 1)
+
+# Try to provide human-readable version of latest commit for dev versions
+# E.g. v0.5.1-4-g49a49f2-wip
+#      * 4 commits after tag v0.5.1
+#      * Latest commit "49a49f2"
+#      * -wip: Working tree is dirty (non committed stuff)
+# See: https://git-scm.com/docs/git-describe
+
+if len(VERSION) == 5:
+    import subprocess
+    try:
+        git_version = subprocess.check_output(["git", "describe",
+                                               "--dirty=-wip"],
+                                              stderr=subprocess.STDOUT)
+        commits_from_tag = git_version.strip().decode('ascii')
+        commits_from_tag = commits_from_tag.split("-")[1]
+        VERSION = VERSION[:4] + (int(commits_from_tag),)
+    except Exception as ex:
+        pass
+
+# PEP440 Format
+__version__ = "%d.%d.%d.%s%d" % VERSION if len(VERSION) == 5 else \
+              "%d.%d.%d" % VERSION[:3]
+
 setup(name='upf_pyperplan',
-      version=upf_pyperplan.__version__,
+      version=__version__,
       description='upf_pyperplan',
       author='UPF Team',
       author_email='info@upf.com',

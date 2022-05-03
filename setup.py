03dbd46bb52aab0dec8e3796d06da0a00c241fe5
Alessandro Valentini <alvalentini@fbk.eu>
2022-05-03 17:11:36 +0200

    Added pyperplan version

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(name='up_pyperplan',
       url='https://www.aiplan4eu-project.eu',
       packages=['up_pyperplan'],
       python_requires='>=3.7',
-      install_requires=['pyperplan'],
+      install_requires=['pyperplan==2.1'],
       license='APACHE'
      )
